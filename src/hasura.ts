fd2b8333a6489ee261c6d5084903b33e59b0ad92
Vinayak Kukreja <vinakuk@amazon.com>
2023-11-15 17:25:49 -0800

    fix build
    Signed-off-by: Vinayak Kukreja <vinakuk@amazon.com>

--- src/hasura.ts
+++ src/hasura.ts
@@ -1,5 +1,5 @@
 import { Duration } from 'cdk8s';
-import * as kplus from 'cdk8s-plus-24';
+import * as kplus from 'cdk8s-plus-27';
 import { Construct } from 'constructs';
 
 export interface HasuraProps {
